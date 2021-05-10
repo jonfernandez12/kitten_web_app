60793e2da7c1e70778bd91e0af9ee69319f5c1c5
Jon Fernandez <jonferuam@gmail.com>
2021-05-10 21:53:07 +0200

    Added posts

---
+++ components/Navigation.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="header">
+        <h1 class="text-title">Posts APP</h1>
+        <nav>
+            <ul>
+                <li>
+                    <nuxt-link to="/">Home</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/posts">Posts</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/about">About</nuxt-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
